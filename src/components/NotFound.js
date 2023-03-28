420890669a97f3b1768036ff22f908e7b9adae6e
tomvesa <47905734+tomvesa@users.noreply.github.com>
2023-03-28 08:35:57 +0200

    Added go to main page link

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,8 +1,17 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 
-const NotFound = () => (
-    <h1>Oh no! Page not found</h1>
-)
+const NotFound = () => {
+    return (
+        <div>
+            <h1>Oh no! Page not found</h1>
+            <ul>    
+                <li><NavLink to="/">Back to Mainpage</NavLink></li>
+            </ul>
+        </div>
+      
+    )
+}
 
 export default NotFound;
