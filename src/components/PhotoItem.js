c42b7ad6da7bf500cc4d8bd4f88bf75ff8de255a
tomvesa <47905734+tomvesa@users.noreply.github.com>
2023-03-28 08:44:21 +0200

    remove comments

--- src/components/PhotoItem.js
+++ src/components/PhotoItem.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const PhotoItem = ({data}) => {
-console.log("photo item ", data);
+
     return (
         <li>
             <img src={`https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}.jpg`} alt="" />
